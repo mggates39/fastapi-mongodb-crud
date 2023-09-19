e267144b95712a7b6dce3485c9c4291b3a415b79
Marshall G. Gates <mggates@gmail.com>
2023-09-18 22:17:38 -0400

    Added CORS Middleware to secure API

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
+
+client_apps = ['http://localhost:3000']  # Our REACT app will be running on this IP and PORT
 
 app = FastAPI()
 app.include_router(student_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_apps,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
